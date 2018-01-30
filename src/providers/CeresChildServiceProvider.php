04c5887414bf025cd74cc1d3a54413625bc0394c
NashwaFayed <35915969+NashwaFayed@users.noreply.github.com>
2018-01-30 13:27:41 +0200

    Update CeresChildServiceProvider.php

--- src/providers/CeresChildServiceProvider.php
+++ src/providers/CeresChildServiceProvider.php
@@ -1,17 +1,18 @@
 <?php
- 
+
 namespace CeresChild\Providers;
- 
+
+
 use Plenty\Plugin\ServiceProvider;
- 
+
 class CeresChildServiceProvider extends ServiceProvider
 {
- 
+
 	/**
 	 * Register the service provider.
 	 */
-	public function register()
+	public function register() 
 	{
- 
+
 	}
 }
