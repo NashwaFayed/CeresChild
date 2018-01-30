01495040b187f1d0d65fef7d3c2cb856123703e4
NashwaFayed <35915969+NashwaFayed@users.noreply.github.com>
2018-01-30 12:16:09 +0200

    Add files via upload

---
+++ src/providers/CeresChildServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace CeresChild\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class CeresChildServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+}
